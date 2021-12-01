22cafbf12f881f241348348401df0171d1ae8f75
Michael Bikovitsky <biko@bikodbg.com>
2021-12-01 12:23:46 +0200

    Implement day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use itertools::Itertools;
 
 const INPUT: &str = include_str!("day1.txt");
 
@@ -9,6 +10,15 @@ fn main() -> Result<()> {
         .filter(|&index| depths[index] > depths[index - 1])
         .count();
     dbg!(depth_increases);
+
+    let window_sum_increases = depths
+        .iter()
+        .tuple_windows()
+        .map(|window: (_, _, _)| window.0 + window.1 + window.2)
+        .tuple_windows()
+        .filter(|sums: &(_, _)| sums.1 > sums.0)
+        .count();
+    dbg!(window_sum_increases);
 
     Ok(())
 }
