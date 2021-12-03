4a8d92fce3e4368e9b34c6140c753711c2f1d4b4
Michael Bikovitsky <biko@bikodbg.com>
2021-12-03 16:10:22 +0200

    Implement day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -3,9 +3,11 @@ use anyhow::Result;
 use aoc2021::util::input_lines;
 
 fn main() -> Result<()> {
-    let numbers = parse_input()?;
+    let mut numbers = parse_input()?;
 
-    power_consumption(&mut numbers.clone());
+    power_consumption(&mut numbers);
+
+    life_support_rating(&mut numbers);
 
     Ok(())
 }
@@ -44,4 +46,59 @@ fn power_consumption(numbers: &mut [u16]) {
 
     let power_consumption = gamma_rate as u32 * epsilon_rate as u32;
     dbg!(power_consumption);
+}
+
+fn life_support_rating(numbers: &mut [u16]) {
+    // Assume that the leading digit in the original input is not 0
+    let digits = count_binary_digits(*numbers.iter().max().unwrap());
+
+    let sort_by_digit = |numbers, digit| sort_by_digit(numbers, digit, digits);
+
+    let (zeroes, ones) = sort_by_digit(numbers, 1);
+    let (mut oxygen, mut co2) = if ones.len() >= zeroes.len() {
+        (ones, zeroes)
+    } else {
+        (zeroes, ones)
+    };
+
+    for digit in 2..digits + 1 {
+        if oxygen.len() > 1 {
+            let (zeroes, ones) = sort_by_digit(oxygen, digit);
+            if ones.len() >= zeroes.len() {
+                oxygen = ones;
+            } else {
+                oxygen = zeroes;
+            }
+        }
+
+        if co2.len() > 1 {
+            let (zeroes, ones) = sort_by_digit(co2, digit);
+            if zeroes.len() <= ones.len() {
+                co2 = zeroes;
+            } else {
+                co2 = ones;
+            }
+        }
+    }
+
+    assert!(oxygen.len() == 1);
+    let oxygen_generator_rating = oxygen[0];
+    dbg!(oxygen_generator_rating);
+
+    assert!(co2.len() == 1);
+    let co2_scrubber_rating = co2[0];
+    dbg!(co2_scrubber_rating);
+
+    let life_support_rating = oxygen_generator_rating as u32 * co2_scrubber_rating as u32;
+    dbg!(life_support_rating);
+}
+
+fn sort_by_digit(numbers: &mut [u16], digit: u32, total_digits: u32) -> (&mut [u16], &mut [u16]) {
+    let mask = 1 << (total_digits - digit);
+
+    numbers.sort_unstable_by_key(|&number| number & mask);
+
+    let partition = numbers.partition_point(|&number| (number & mask) == 0);
+
+    numbers.split_at_mut(partition)
 }
