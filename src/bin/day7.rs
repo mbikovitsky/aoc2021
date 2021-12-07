56e28ebd183f2019111461e6f662c95f86543478
Michael Bikovitsky <biko@bikodbg.com>
2021-12-07 09:31:26 +0200

    Implement day 7 part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -19,12 +19,33 @@ fn main() -> Result<()> {
     let fuel = calculate_total_distance(&positions, optimal_position);
     dbg!(fuel);
 
+    let optimal_position_demo2 = find_optimal_position2(&demo_positions);
+    dbg!(optimal_position_demo2);
+
+    let fuel_demo2 = calculate_total_fuel2(&demo_positions, optimal_position_demo2);
+    dbg!(fuel_demo2);
+
+    let optimal_position2 = find_optimal_position2(&mut positions);
+    dbg!(optimal_position2);
+
+    let fuel2 = calculate_total_fuel2(&positions, optimal_position2);
+    dbg!(fuel2);
+
     Ok(())
 }
 
 fn find_optimal_position(positions: &mut [u32]) -> u32 {
     let (_, &mut median, _) = positions.select_nth_unstable((positions.len() - 1) / 2);
     median
+}
+
+fn find_optimal_position2(positions: &[u32]) -> u32 {
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    (min..=max)
+        .min_by_key(|candidate| calculate_total_fuel2(positions, *candidate))
+        .unwrap()
 }
 
 fn calculate_total_distance(positions: &[u32], target: u32) -> u32 {
@@ -36,6 +57,23 @@ fn calculate_total_distance(positions: &[u32], target: u32) -> u32 {
             } else {
                 target - position
             }
+        })
+        .sum()
+}
+
+fn calculate_total_fuel2(positions: &[u32], target: u32) -> u32 {
+    positions
+        .iter()
+        .map(|&position| {
+            let distance = if position >= target {
+                position - target
+            } else {
+                target - position
+            };
+
+            let fuel = (0 + distance) * (distance + 1) / 2;
+
+            fuel
         })
         .sum()
 }
