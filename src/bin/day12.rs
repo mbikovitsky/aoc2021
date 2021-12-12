b3f34776c66d6b236e8678a84e381b3d9d22eea4
Michael Bikovitsky <biko@bikodbg.com>
2021-12-12 15:13:27 +0200

    Implement day 12 part 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -10,11 +10,18 @@ fn main() -> Result<()> {
     let paths = count_paths(&graph);
     dbg!(paths);
 
+    let paths2 = count_paths2(&graph);
+    dbg!(paths2);
+
     Ok(())
 }
 
 fn count_paths(graph: &HashMap<String, Vec<String>>) -> u64 {
-    fn backtrack<'a>(graph: &'a HashMap<String, Vec<String>>, current: &str, visited: &mut HashSet<&'a str>) -> u64 {
+    fn backtrack<'a>(
+        graph: &'a HashMap<String, Vec<String>>,
+        current: &str,
+        visited: &mut HashSet<&'a str>,
+    ) -> u64 {
         if current == "end" {
             return 1;
         }
@@ -40,6 +47,56 @@ fn count_paths(graph: &HashMap<String, Vec<String>>) -> u64 {
 
     let mut visited = HashSet::from(["start"]);
     backtrack(graph, "start", &mut visited)
+}
+
+fn count_paths2(graph: &HashMap<String, Vec<String>>) -> u64 {
+    fn backtrack<'a>(
+        graph: &'a HashMap<String, Vec<String>>,
+        current: &str,
+        visited: &mut HashSet<&'a str>,
+        small_cave_visited_twice: bool,
+    ) -> u64 {
+        if current == "end" {
+            return 1;
+        }
+
+        let mut count = 0;
+
+        for neighbour in graph.get(current).unwrap() {
+            let is_small_cave = neighbour.chars().all(|c| c.is_lowercase());
+
+            let neighbour_visited = visited.contains(neighbour.as_str());
+
+            let mut inserted = false;
+
+            let mut small_cave_visited_twice = small_cave_visited_twice;
+
+            if is_small_cave {
+                if neighbour_visited {
+                    if small_cave_visited_twice || neighbour == "start" {
+                        continue;
+                    }
+                    small_cave_visited_twice = true;
+                } else {
+                    visited.insert(neighbour);
+                    inserted = true;
+                }
+            } else {
+                assert!(!neighbour_visited);
+            }
+
+            count += backtrack(graph, neighbour, visited, small_cave_visited_twice);
+
+            if inserted {
+                visited.remove(neighbour.as_str());
+            }
+        }
+
+        count
+    }
+
+    let mut visited = HashSet::from(["start"]);
+    backtrack(graph, "start", &mut visited, false)
 }
 
 fn parse_input() -> Result<HashMap<String, Vec<String>>> {
